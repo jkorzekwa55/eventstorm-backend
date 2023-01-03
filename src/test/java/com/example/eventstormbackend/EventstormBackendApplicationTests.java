ff84b7c09ae7c3e1ce25c1207635a307c303acde
Julia Korzekwa <julia0n3@gmail.com>
2023-01-03 20:24:52 +0100

    initial commit

---
+++ src/test/java/com/example/eventstormbackend/EventstormBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventstormbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventstormBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
